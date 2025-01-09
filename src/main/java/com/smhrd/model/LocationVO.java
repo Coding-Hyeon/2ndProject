a1d55709266c5fbfaf34e010a745706ec947f36e
jangoseung <jangoseung@gmail.com>
2025-01-09 14:51:28 +0900

    - locationPostProcessServlet - MapServlet - LocationDAO.java - LocationVO.java - createLocationPost.jsp - partyRoom.jsp -<dependency>gson

---
+++ src/main/java/com/smhrd/model/LocationVO.java
@@ -0,0 +1,41 @@
+package com.smhrd.model;
+
+public class LocationVO {
+    private String title; // l_post_nm
+    private String content; // l_post_content
+    private double latitude; // latitude
+    private double longitude; // longitude
+
+    // Getter & Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
